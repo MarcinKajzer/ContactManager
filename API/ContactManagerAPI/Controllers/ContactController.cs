8ec61ec753f6e6fd48ac478f2432a66ae15e9877
Marcin Kajzer <kajzer-marcin@wp.pl>
2022-09-03 17:39:23 +0200

    ADD contact controller and create action.

---
+++ API/ContactManagerAPI/Controllers/ContactController.cs
@@ -0,0 +1,53 @@
+﻿using ContactManagerAPI.DTOs;
+using ContactManagerAPI.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactManagerAPI.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly AppDbContext _dbContext;
+        public ContactController(AppDbContext context)
+        {
+            _dbContext = context;
+        }
+
+        [HttpPost]
+        [Route("Contacts")]
+        public IActionResult Create(CreateContactDTO contactDTO)
+        {
+            //Model validation acording to properies atributes
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            //Map DTO to Contact entity
+            Contact contact = new Contact()
+            {
+                Email = contactDTO.Email,
+                FirstName = contactDTO.FirstName,
+                LastName = contactDTO.LastName,
+                PhoneNumber = contactDTO.PhoneNumber,
+                DateOfBirth = contactDTO.DateOfBirth,
+                CategoryId = contactDTO.CategoryId,
+                SubCategoryId = contactDTO.SubCategoryId
+            };
+
+            //Save new contact to database via dbContext
+            try
+            {
+                _dbContext.Contacts.Add(contact);
+                _dbContext.SaveChanges();
+            }
+            catch
+            {
+                return BadRequest(new
+                {
+                    errorMessage = "Cannot create new contact."
+                }); ;
+            }
+
+            //Return status 200 after succesfull creation
+            return Ok(contact);
+        }
+    }
+}
---
+++ API/ContactManagerAPI/DTOs/CreateContactDTO.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactManagerAPI.DTOs
+{
+    public class CreateContactDTO
+    {
+        [Required]
+        [MaxLength(100)]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string LastName { get; set; }
+
+        [Required]
+        [MaxLength(9)]
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+        public int SubCategoryId { get; set; }
+    }
+}
