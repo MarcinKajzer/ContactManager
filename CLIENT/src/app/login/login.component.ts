ded5fc92c9e9a8604f7994cd15c500a97552bf4b
Marcin Kajzer <kajzer-marcin@wp.pl>
2022-09-04 13:08:39 +0200

    ADD login and register base components

---
+++ CLIENT/src/app/login/login.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent{
+
+  loginData = {
+    email: '',
+    password: ''
+  }
+
+  confirm(){
+    console.log(this.loginData)
+  }
+
+}
